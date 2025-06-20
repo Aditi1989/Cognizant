e6de07a087ead6499df50b8826f7f8aa4e7f29ed
Aditi1989 <154396307+Aditi1989@users.noreply.github.com>
2025-06-21 00:03:36 +0530

    Add week 1 Java exercises

---
+++ week 1/SingletonPatternExample/loggertest.java
@@ -0,0 +1,11 @@
+public class loggertest {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First log message");
+        logger2.log("Second log message");
+
+        System.out.println(logger1 == logger2); // true
+    }
+}
